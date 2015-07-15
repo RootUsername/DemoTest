8a385ed72971cfa289211a7ea2f3579bc60f931e
RootUsername <rellame@sina.com>
2015-07-15 08:25:08 +0800

    create the project

---
+++ DemoTest/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  DemoTest
+//
+//  Created by boya on 15/7/14.
+//  Copyright (c) 2015年 zbj. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ DemoTest/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  DemoTest
+//
+//  Created by boya on 15/7/14.
+//  Copyright (c) 2015年 zbj. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
